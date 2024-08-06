a77c836092d5414ca1e30cec439e2f71412a1d4f
System Administrator <admin@example.com>
2024-08-06 11:09:04 -0700

    table: synced with metadata

--- src/fluent/t2t.table.now.ts
+++ src/fluent/t2t.table.now.ts
@@ -1,16 +1,16 @@
 import '@servicenow/sdk-core/global'
 import { Table, StringColumn } from '@servicenow/sdk/core'
 
-export const sn_t2f_fluent_t2t_kbd = Table({
-    name: 'sn_t2f_fluent_t2t_kbd',
+export const sn_t2f_fluent_kbd = Table({
+    name: 'sn_t2f_fluent_kbd',
     label: 'Text2Test Fluent Knowledge Base Document',
-    extends: 'task',
+    extends: '',
     schema: {
-        topic: StringColumn({ label: 'Topic', maxLength: 40 }),
-        reference: StringColumn({ label: 'Reference', maxLength: 1024 }),
         name: StringColumn({ label: 'Name', mandatory: true, maxLength: 40 }),
         content: StringColumn({ label: 'Content', mandatory: true, maxLength: 4096 }),
         category: StringColumn({ label: 'Category', mandatory: true, maxLength: 40 }),
+        topic: StringColumn({ label: 'Topic', maxLength: 40 }),
+        reference: StringColumn({ label: 'Reference', maxLength: 1024 })
     },
     display: 'name',
     allow_client_scripts: true,
